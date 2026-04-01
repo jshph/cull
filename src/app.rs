28fb8a6e11832c3a9424a56ed60140b1325fb60f
Joshua Pham <joshua.pham@gmail.com>
2026-04-01 00:18:30 -0400

    Wire Lemon Squeezy checkout URL into Buy license button

--- src/app.rs
+++ src/app.rs
@@ -995,7 +995,7 @@ impl CullApp {
                         }
                         if ui.button("Buy license").clicked() {
                             let _ = std::process::Command::new("open")
-                                .arg("https://getcull.fyi#buy")
+                                .arg("https://cull.lemonsqueezy.com/checkout/buy/3345aefa-2fc3-460c-bc67-d2afdcab29e5")
                                 .spawn();
                         }
                     });
